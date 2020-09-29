d06e73968f4624067c23052249fa85260dec876a
alex-tsy <68004960+alex-tsy@users.noreply.github.com>
2020-09-29 15:08:15 +0300

    init:start task 'typical arrays problems' and add solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
 
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array) {
+    if (array == undefined) {
+        return 0;
+    }
+    return array.reduce((acc, e) => Math.min(acc, e), 0);
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+    if (array == undefined) {
+        return 0;
+    }
+    return array.reduce((acc, e) => Math.max(acc, e), 0);
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+    if (array == undefined || array.length == 0) {
+        return 0;
+    }
+    return array.reduce((sum, e) => sum + e, 0) / array.length;
 }
